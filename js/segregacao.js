c43c48a1598564c3643e0f58e62315adf2c75ddb
Jonatas "Solus" Canuto <jonatas.ac@hotmail.com>
2023-11-02 16:15:37 -0300

    Solus
    update

---
+++ js/segregacao.js
@@ -0,0 +1,9 @@
+console.log("Olá Mundo, Bem vindo a JS");
+console.log(`Data hora: ${new Date()}`);
+function mensagem() {
+  alert("Chamamos um modal");
+}
+
+function mensagem2(){
+    alert('Mensagem 2');
+}
